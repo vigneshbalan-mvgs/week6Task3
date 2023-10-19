4aa26f78e2f1c34ee8c2a1229eba6cdcd38aae87
MVGS <vigneshbalanmvgs2003@gmail.com>
2023-10-19 07:02:19 +0530

    3.0

--- check.js
+++ check.js
@@ -1,31 +1,24 @@
-// Get the DOM elements for the username, password, and login button fields.
-const uname = document.getElementById('uname');
-const pwd = document.getElementById('pwd');
-const loginBtn = document.getElementById('loginFormBtn');
+//login form
 
-// Disable the login button by default.
+const uname = document.getElementById('uname');
+  const pwd = document.getElementById('pwd');
+  const loginBtn = document.getElementById('loginFormBtn');
 loginBtn.disabled = true;
 
-// Create a function to check the password and username fields.
-function checkPassword() {
-  // Check if the password and username fields match the values stored in the password and username variables.
-  if (pwd.value == password ) {
-    // Enable the login button.
-    if( uname.value == username){
-
-      loginBtn.disabled = false;
-    }
-    else{
-
-      uname.addEventListener('keyup', checkPassword);
-    }
-  } else {
-    // Disable the login button.
-    loginBtn.disabled = true;
-  }
+ 
   
+function checkPassword(){ 
+  if(uname!=""){
+    signupFormBtn.disabled= false;
+  }
+  else{
+    signupFormBtn.disabled= true;
+  }
 }
+password.addEventListener("keyup",()=>{
+    if(password.value.length !=0){
+        checkPassword();
+    }
+}) 
 
-
-// Add an event listener to the password field that calls the checkPassword() function whenever the user releases a key.
-pwd.addEventListener('keyup', checkPassword);
+passwordConfirm.addEventListener("keyup",checkPassword);
